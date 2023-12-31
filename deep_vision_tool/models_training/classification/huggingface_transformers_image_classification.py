2e2fd286a7f5c9b2826c7e1782a52fadc0b74247
Anupam Nautiyal <nautiyalanupam98@gmail.com>
2023-12-31 16:22:19 +0530

    added gitignore

--- deep_vision_tool/models_training/classification/huggingface_transformers_image_classification.py
+++ deep_vision_tool/models_training/classification/huggingface_transformers_image_classification.py
@@ -1,1 +1,13 @@
 from transformers import AutoFeatureExtractor, AutoModelForImageClassification
+
+class FeatureExtraction:
+    def __init__(self) -> None:
+        pass
+
+class ImageClassificationTransformer:
+    def __init__(self, pretrained_model: str) -> None:
+        self.pretrained_model =  pretrained_model
+        self.auto_feature = AutoFeatureExtractor.from_pretrained(self.pretrained_model)
+        self.model        = AutoModelForImageClassification.from_pretrained(self.pretrained_model)
+
+ImageClassificationTransformer("google/vit-base-patch16-224")
